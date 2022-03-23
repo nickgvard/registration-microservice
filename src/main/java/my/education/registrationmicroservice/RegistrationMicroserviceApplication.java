143eeb43e0075defe21392ca734a6ce88ebd6cb5
nickgvard <project.3players@gmail.com>
2022-03-23 11:22:55 +0300

    Init commit

---
+++ src/main/java/my/education/registrationmicroservice/RegistrationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package my.education.registrationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationMicroserviceApplication.class, args);
+    }
+
+}
